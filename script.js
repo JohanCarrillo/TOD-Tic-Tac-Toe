5f4638e5d7b428ea4f1180fa4a795bda57d5a3d2
JohanCarrillo <johanime1907@gmail.com>
2022-01-07 23:32:58 -0500

    started project!

---
+++ script.js
@@ -0,0 +1,17 @@
+(function gameBoard() {
+	const board = new Array(9);
+
+	function play(index, player) {};
+
+	return {board};
+})();
+
+(function displayController() {
+
+})();
+
+function player(name, symbol) {
+
+	return {name, symbol};
+
+};
